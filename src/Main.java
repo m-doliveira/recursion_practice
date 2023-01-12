11f7a77eecb95c4ac6e200efb213d9ac9d243a80
m-doliveira <monica_doliveira24@milton.edu>
2023-01-12 09:39:33 -0500

    end of homework 1.12.23

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,16 @@ public class Main {
         int a;
 factorials(4);
     }
+
     public int factorials(int a){
         int fac=a;
-        System.out.println(fac);
-        if (a>=1) {
-return fac*(a-1);
+        if (a==0) {
+            return 1;
+
         }
         else{
             factorials(a-1);
-
+            return fac*(a-1);
         }
-
-        return fac;
     }
 }
